216e420a2d57bc0a9ca3c4d8bdee7fe964911afd
Jay Thakur <jay.thakur.sdb@gmail.com>
2026-01-23 14:35:36 +0530

    Remove tailwind css errors from rspec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Prevent Propshaft from blowing up in tests
+  config.assets.unknown_asset_fallback = true
 end
